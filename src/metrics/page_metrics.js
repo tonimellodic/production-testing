795ac96414df26100f9d0c2c4e1659423d04dd18
tonimellodic <tonitingtong@gmail.com>
2019-03-15 12:54:17 +0100

    add monitoring to google test

---
+++ src/metrics/page_metrics.js
@@ -0,0 +1,34 @@
+const performanceTiming = page => {
+  return page
+    .evaluate(() => JSON.stringify(window.performance.timing))
+    .then(timing => JSON.parse(timing))
+}
+
+const getDifferenceOfMetrics = (page, startMetric, endMetric) => {
+  const msInASecond = 1000
+  return performanceTiming(page).then(
+    timing => (timing[endMetric] - timing[startMetric]) / msInASecond
+  )
+}
+
+const domComplete = (page, options) => {
+  return page.waitForFunction(
+    'window.performance.timing.domComplete > 0',
+    options
+  )
+}
+
+const getLoadDuration = (page, options) => {
+  return domComplete(page, options).then(() =>
+    getDifferenceOfMetrics(page, 'navigationStart', 'loadEventEnd')
+  )
+}
+
+const getActionDuration = page => {
+  return getDifferenceOfMetrics(page, 'requestStart', 'responseEnd')
+}
+
+module.exports = {
+  getLoadDuration,
+  getActionDuration
+}
--- test/google.test.js
+++ test/google.test.js
@@ -1,10 +1,19 @@
 const GooglePage = require('../src/pages/google')
+const { getLoadDuration } = require('../src/metrics/page_metrics')
 
 describe('google', () => {
   const options = { timeout: 20000 }
   const googlePage = new GooglePage(page, options) // page already initialised
 
+  const reportMetrics = duration => {
+    return console.log(`google page took ${duration}s to load`)
+  }
+
   it('loads google page', () => {
-    return googlePage.goto().then(googlePage.waitForPageLoaded)
+    return googlePage
+      .goto()
+      .then(googlePage.waitForPageLoaded)
+      .then(() => getLoadDuration(page, googlePage.options))
+      .then(reportMetrics)
   })
 })
